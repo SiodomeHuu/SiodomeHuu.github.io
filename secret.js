b443ec2f16f5f2233e33de0794d0ed6c6bf3d4bc
jikaiwen <153139310@qq.com>
2018-01-06 13:42:58 +0800

    Add salt before MD5

--- secret.js
+++ secret.js
@@ -1,6 +1,6 @@
 function _exec(tp) {
-	tp_md5=hex_md5(tp);
-	if(  tp_md5 == "0fadf2c580496ffce264bb6e7ffb1f99" ) { // change this
+	tp_md5=hex_md5(tp+"secret code's salt!!!");
+	if(  tp_md5 == "894898b37be3e46c670c7d7d86daf468" ) { // change this
 		window.location.href="http://home.ustc.edu.cn/~jkw/secret/"+tp+".html";
 	}
 	else if(tp=="") {
